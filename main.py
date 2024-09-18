75f9b8be1b2c48b5958b4aa0c4a8b0938c63fbc0
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-09-18 17:38:25 +0800

    first commit with bug

---
+++ main.py
@@ -0,0 +1,11 @@
+def add_numbers(num1, num2):
+  """Adds two numbers and returns the result."""
+  return num1 + num2
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+sum = add_numbers(num1, num2)
+print("The sum of", num1, "and", num2, is:, sum)
