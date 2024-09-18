cbcdaeaa467390ad2eb832f071f8882a16beed96
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-09-18 17:44:32 +0800

    branch3 fix sng bug

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ num2 = float(input("Enter the second number: "))
 
 
 sum = add_numbers(num1, num2)
-print("The sum of", num1, "and", num2, is:, sum)
+print("The sum of", num1, "and", num2, "is":, sum)
